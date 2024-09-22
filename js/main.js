38c7d8abfafa926c900721b82ca29300117547c5
Uzeyr Navapurwala <uzeyr.nav@gmail.com>
2024-09-22 12:42:03 +0100

    added comments

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 //Pokemon API
-document.querySelector("button").addEventListener("click", getPokemon); //add functional;it to buttton tied to getPokemon function(button)
 
+//! DOM Manipulation //!
+document.querySelector("button").addEventListener("click", getPokemon);
+//add functionality to 'buttton' tied to getPokemon function.
+
+  
 
 function getPokemon() {
   //console.log('does this work') yes
-  
-  let userChoice = document.querySelector("input").value;
-  const url = `https://pokeapi.co/api/v2/pokemon/${userChoice}`.toLowerCase(); //pokemon api
-  const getImage = `https://img.pokemondb.net/artwork/${userChoice}.jpg`;
 
-  
+  let userChoice = document.querySelector("input").value;
+  //waits for input to get the VALUE from user use choice
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${userChoice}`.toLowerCase();
+  //pokemon api data stored in varaible for the fetch
+  const getImage = `https://img.pokemondb.net/artwork/${userChoice}.jpg`;
+  //image sprite from seperate sources not an api
 
   fetch(url)
-    .then((res) => res.json()) //parse the url (api) in json format and stores in var res(respond)
+    .then((res) => res.json()) //parse the url (api) in json format and stores in varibale res(respond)
     .then((res) => {
       // respond in console as below
-      //console.log(res.types)
-      document.querySelector("h1").innerText = res.name;
+      console.log(res)
+      document.querySelector("h1").innerText = res.name; 
+        //!DOM manipulation getting the data direclt from the api and putting in the dom //! 
       document.querySelector(".pokemonImage").src = getImage;
 
-      let pokemonType = res.types;
+      let pokemonType = res.types; 
+      //Access array on objects for additional information such as types of elements, stored in varible pokemontYpes.
+
+      //for each function
       pokemonType.forEach((e) => {
-        let typeList = document.createElement("li"); //var typelist = create a new element in html 
-        typeList.innerText = e.type.name; // 
+        let typeList = document.createElement("li"); //var typelist = create a new element in html
+        typeList.innerText = e.type.name; //
         document.querySelector("#typeList").appendChild(typeList);
       });
     })

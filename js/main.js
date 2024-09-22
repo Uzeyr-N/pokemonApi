c20352e5f7fe340d2fd7a110e10f4f2d440664d2
Uzeyr Navapurwala <uzeyr.nav@gmail.com>
2024-09-22 11:38:25 +0100

    add new features and updated syntaxt

--- js/main.js
+++ js/main.js
@@ -1,144 +1,70 @@
-//Code 01
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(){
-//     console.log('Paper delivered to house 2')
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo()
-// houseThree()
+//Pokemon API
+document.querySelector("button").addEventListener("click", getPokemon); //add functional;it to buttton tied to getPokemon function(button)
 
-//Code 02
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(){
-//     setTimeout(() => console.log('Paper delivered to house 2'), 3000)
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo()
-// houseThree()
 
-//Code 03
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(callback){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 2')
-//         callback()
-//     }, 3000)
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo(houseThree)
+function getPokemon() {
+  //console.log('does this work') yes
+  
+  let userChoice = document.querySelector("input").value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${userChoice}`.toLowerCase(); //pokemon api
+  const getImage = `https://img.pokemondb.net/artwork/${userChoice}.jpg`;
 
-//Code 04
-// function houseOne(){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 1')
-//         setTimeout(() => {
-//             console.log('Paper delivered to house 2')
-//             setTimeout(() => {
-//                 console.log('Paper delivered to house 3')
-//             }, 3000)
-//         }, 4000)
-//     }, 5000)
-// }
-// houseOne()
+  
 
-//Code 05
-// const promise = new Promise((resolve, reject) => {
-//     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
-//     }else{
-//         reject('Error: Operation has failed')
-//     }
-// })
-// console.log(promise)
-// promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+  fetch(url)
+    .then((res) => res.json()) //parse the url (api) in json format and stores in var res(respond)
+    .then((res) => {
+      // respond in console as below
+      //console.log(res.types)
+      document.querySelector("h1").innerText = res.name;
+      document.querySelector(".pokemonImage").src = getImage;
 
-//Code 06
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
-// }
-// houseOne()
-//     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+      let pokemonType = res.types;
+      pokemonType.forEach((e) => {
+        let typeList = document.createElement("li"); //var typelist = create a new element in html 
+        typeList.innerText = e.type.name; // 
+        document.querySelector("#typeList").appendChild(typeList);
+      });
+    })
+    .catch((err) => {
+      console.log("error");
+    });
+}
 
-//Code 07
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
-// }
+// const apikey = "https://pokeapi.co/api/v2/move";
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
+// fetch(apikey)
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//   })
+
+//   .catch((err) => {
+//     console.log("second one not working");
+//   });
+
+// document.querySelector('button').addEventListener('click', getPokemon);
+
+// function getPokemon(){
+//     const userChoice = document.querySelector('input').value.trim().toLowerCase(); // User input (name)
+//     const url = `https://pokeapi.co/api/v2/pokemon/${userChoice}`;
+//     const getImage = `https://img.pokemondb.net/artwork/${userChoice}.jpg`; // Image URL
+//     const abilities =
+
+//     fetch(url)
+//         .then(pokemon => {
+//             if (!pokemon.ok) {
+//                 throw new Error('Pokemon not found');
+//             }
+//             return pokemon.json();
+//         })
+//         .then(pokemon => {
+//             document.querySelector('h1').innerText = pokemon.name; // Display Pokémon name
+//             document.querySelector('img').src = getImage;  // Set image source
+//         })
+//         .catch(err => {
+//            // document.querySelector('h1').innerText = "Pokemon not found";
+//             //document.querySelector('#pokemon-img').src = ""; // Clear image if not found
+//             console.log('Error:', err);
+//         });
 // }
-
-// getPaid()
-
-//Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
-
